8396e454b5b4ad215cb61d1fc93225eb1b724fdd
Floyd Hightower <floyd.hightower27@gmail.com>
2019-02-09 22:03:59 -0400

    Importing packages

--- te_python/__init__.py
+++ te_python/__init__.py
@@ -4,3 +4,5 @@
 __author__ = """Floyd Hightower"""
 __email__ = 'floyd.hightower27@gmail.com'
 __version__ = '0.1.0'
+
+from te_python.te_python import email_submit, email_add_analysis, email_add_hash, email_get_details, email_tlsh_hash
