9803114e386aebea91a19e54ff92719baf3f7de1
Floyd Hightower <floyd.hightower27@gmail.com>
2019-02-09 22:01:24 -0400

    Adding functions for adding email analysis and adding hashes

--- te_python/te_python.py
+++ te_python/te_python.py
@@ -5,6 +5,8 @@
 import os
 import sys
 
+import requests
+
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), ".")))
 import utility
 
@@ -18,11 +20,34 @@ def email_submit(email_text):
         "full_text": email_text
     }
 
-    return utility.make_request(url, data=email_data, return_json=True)
+    return utility.make_request(requests.post, url, data=email_data)
 
 
 def email_get_details(email_id):
     """Get details about the email with the given email ID."""
     url = BASE_API_URL.format('emails/{}'.format(email_id))
 
-    return utility.make_request(url)
+    return utility.make_request(requests.get, url)
+
+
+def email_add_analysis(api_token, email_id, analysis_data):
+    """(AUTHENTICATION REQUIRED) Create a new analysis result."""
+    url = BASE_API_URL.format('emails/{}/analysis'.format(email_id))
+    headers = utility.create_header()
+
+    return utility.make_request(requests.get, url, data=analysis_data, headers=headers)
+
+
+def email_add_hash(api_token, email_id, hash_type, hash_value):
+    """(AUTHENTICATION REQUIRED) Add a hash to the email."""
+    url = BASE_API_URL.format('emails/{}'.format(email_id))
+    headers = utility.create_header()
+
+    return utility.make_request(requests.put, url, data={
+        hash_type: hash_value
+    }, headers=headers)
+
+
+def email_tlsh_hash(api_token, email_id, tlsh_hash):
+    """(AUTHENTICATION REQUIRED) Add a TLSH hash to the email."""
+    return email_add_hash(api_token, email_id, 'tlsh_hash', tlsh_hash)
--- te_python/utility.py
+++ te_python/utility.py
@@ -6,13 +6,23 @@ import json
 import requests
 
 
-def make_request(url, data=None, return_json=False):
+def create_header(api_token):
+    return {'Authorization': 'Token {}'.format(api_token)}
+
+
+def make_request(function, url, data=None, headers={}, return_json=True):
     """Make a request to the given site."""
-    response = requests.post(url, data=json.dumps(data))
+    if data:
+        response = function(url, data=json.dumps(data), headers=headers)
+    else:
+        response = function(url, headers=headers)
 
     if not response.ok:
         message = 'ERROR: Got {} response from {}: {}'.format(response.status_code, url, response.text)
         print(message)
         return None
     else:
-        return json.loads(response.text)
+        if return_json:
+            return json.loads(response.text)
+        else:
+            return response.text
