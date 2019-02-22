f1e69800fe46d2f38986813ff464100a3e51f261
Floyd Hightower <floyd.hightower27@gmail.com>
2019-02-22 06:32:17 -0400

    Adding trailing slashes

--- te_python/te_python.py
+++ te_python/te_python.py
@@ -17,14 +17,14 @@ def email_submit(email_text, base_api_url=base_api_url):
 
 def email_get_details(email_id, base_api_url=base_api_url):
     """Get details about the email with the given email ID."""
-    url = base_api_url.format('emails/{}'.format(email_id))
+    url = base_api_url.format('emails/{}/'.format(email_id))
 
     return make_request(requests.get, url)
 
 
 def email_add_analysis(api_token, email_id, analysis_notes, analysis_source, base_api_url=base_api_url):
     """(AUTHENTICATION REQUIRED) Create a new analysis result."""
-    url = base_api_url.format('emails/{}/analysis'.format(email_id))
+    url = base_api_url.format('emails/{}/analysis/'.format(email_id))
 
     data = {
         'notes': analysis_notes,
@@ -38,7 +38,7 @@ def email_add_analysis(api_token, email_id, analysis_notes, analysis_source, bas
 
 def email_add_hash(api_token, email_id, hash_type, hash_value, base_api_url=base_api_url):
     """(AUTHENTICATION REQUIRED) Add a hash to the email."""
-    url = base_api_url.format('emails/{}'.format(email_id))
+    url = base_api_url.format('emails/{}/'.format(email_id))
 
     return make_request(requests.put, url, data={hash_type: hash_value}, api_token=api_token)
 
