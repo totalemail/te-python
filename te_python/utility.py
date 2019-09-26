e6aa4738078c36469133fdcfa40a7c3c6e9f45c9
Floyd Hightower <floyd.hightower27@gmail.com>
2019-09-26 09:07:23 -0400

    Reading config from local file

--- te_python/utility.py
+++ te_python/utility.py
@@ -1,37 +1,83 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+try:
+    import ConfigParser
+except:
+    import configparser as ConfigParser
 import json
+
+try:
+    import urllib.parse as urlparse
+except ImportError:
+    import urlparse
 
 import requests
 
-base_api_url = 'https://totalemail.io/api/v1/{}'
+CONFIG_FILE_PATH = '~/.totalemail/conf'
+DEFAULT_BASE_API_URL = 'https://totalemail.io/api/v1/'
 
 
-def create_header(api_token=None):
+def _read_config_file():
+    """Read the config file with configuration details."""
+    api_token = None
+    api_base_url = DEFAULT_BASE_API_URL
+
+    config = ConfigParser.RawConfigParser()
+    config.read(CONFIG_FILE_PATH)
+
+    default_config = config['DEFAULT']
+
+    if default_config.get('api_token'):
+        api_token = default_config['api_token']
+
+    if default_config.get('api_base_url'):
+        api_base_url = default_config['api_base_url']
+
+    return api_token, api_base_url
+
+
+def init():
+    """Collect the data required to make a request to TE."""
     headers = {'Content-type': 'application/json'}
+
+    api_token, api_base_url = _read_config_file()
 
     if api_token:
         headers['Authorization'] = 'Token {}'.format(api_token)
 
-    return headers
-
-
-def make_request(function, url, data=None, return_json=True, api_token=None):
-    """Make a request to the given site."""
-    headers = create_header(api_token)
-
-    if data:
-        response = function(url, data=json.dumps(data), headers=headers)
+    if api_base_url is None:
+        message = 'The api_base_url entry in the config file ({}) has no value. Please provide a valid value or remove the api_base_url entry to use the default API ({}).'.format(
+            CONFIG_FILE_PATH, DEFAULT_BASE_API_URL
+        )
+        raise RuntimeError(message)
     else:
-        response = function(url, headers=headers)
+        # if there is a api_base_url, make sure it ends with a '/'
+        if not api_base_url.endswith('/'):
+            api_base_url = api_base_url + '/'
 
-    if not response.ok:
-        message = 'ERROR: Got {} response from {}: {}'.format(response.status_code, url, response.text)
-        print(message)
-        return {'status': 'failure', 'message': message}
-    else:
-        if return_json:
-            return {'status': 'success'}
-        else:
-            return response.text
+    return headers, api_base_url
+
+
+def make_get_request(url_path):
+    """Make a GET request to the given URL."""
+    url = urlparse.urljoin(BASE_API_URL, url_path)
+    response = requests.get(url, headers=HEADERS)
+    return response
+
+
+def make_post_request(url_path, data):
+    """Make a POST request to the given URL."""
+    url = urlparse.urljoin(BASE_API_URL, url_path)
+    response = requests.post(url, data=json.dumps(data), headers=HEADERS)
+    return response
+
+
+def make_put_request(url_path, data):
+    """Make a PUT request to the given URL."""
+    url = urlparse.urljoin(BASE_API_URL, url_path)
+    response = requests.put(url, data=json.dumps(data), headers=HEADERS)
+    return response
+
+
+HEADERS, BASE_API_URL = init()
